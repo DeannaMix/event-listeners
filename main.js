6b35f830ba9b76ef3e1e1748943f88a071370604
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-07-27 19:01:47 -0500

    event stuff

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log("Deanna")
+// console.log("Deanna")
+
+// function hello(x) {
+//     return 'Hello ${x}';
+//   };
+//   console.log(hello(world))
+
+// function bye(y) {
+//     return 'Bye, ${y}';
+//   };
+//   console.log(bye(world))
+
+
+//   function third(hello, bye) {
+
+//   }
+
+  const button = document.createElement('main-btn');
+  console.log(button);
+
+  let myBtn = document.querySelector("#main-btn")
+  console.log(myBtn);
+
+  div.addEventListener(click, () => {
+      console.log('mouse has entered!');
+  });
+
+  const button = document.getElementById('main-btn');
+
+  const whatIsTheId = (e) => {
+      return `the element is is ${e.target.id}`;
+  }
+
+  button.addEventListener(click, whatIsTheId);
+
+// const click = (here) => {
+//     return 'You ${here} me!'
+}
+const otherBtn = document.querySelector('#main-btn')
+
+const youClickedMe(click)
